35521ddfc68d7633a316183ac07b5454f73577cd
김종혁 <hwaseen@gmail.com>
2020-05-08 23:00:55 +0900

    TodoForm 인풋 개선 onKeyDown 이벤트 추가하여 엔터 눌러도 todo 생성 가능하게 개선

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -8,21 +8,30 @@ interface TodoFormProps {
 function TodoForm({ addTodo }: TodoFormProps) {
   const [inputText, setInputText] = useState<string>("");
 
+  const handleSubmit = () => {
+    addTodo(inputText);
+    setInputText("");
+  };
+
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInputText(e.target.value);
   };
 
-  const handleSubmit = () => {
-    addTodo(inputText);
+  const handleKeySubmit = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
   };
 
   return (
     <div className="todo-form">
       <input
         className="todo-form-input"
-        onChange={(e) => handleInput(e)}
         type="text"
         placeholder="할 일을 입력해 주세요."
+        value={inputText}
+        onChange={handleInput}
+        onKeyPress={handleKeySubmit}
       />
       <button className="todo-form-btn" onClick={handleSubmit}>
         확인
