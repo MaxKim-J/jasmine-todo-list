00d582a19eb13c537647c095e77279fb8014f1e0
김종혁 <hwaseen@gmail.com>
2020-05-08 19:31:15 +0900

    App 컴포넌트에서 전역 데이터, 함수 구현 @types 폴더 만들고 전역에서 쓸 Todo 타입 선언

---
+++ src/@types/todo.ts
@@ -0,0 +1,7 @@
+interface Todo {
+  id: number;
+  content: string;
+  isChecked: boolean;
+}
+
+export default Todo;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTitle from "./components/TodoTitle";
+import Todo from "./@types/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (content: string) => {
+    const newTodo: Todo = {
+      id: todos.length,
+      content: content,
+      isChecked: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id: number) => {
+    const newTodos: Todo[] = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const checkTodo = (id: number) => {
+    const newTodos = [...todos];
+    newTodos.forEach((todo) => {
+      if (todo.id === id) {
+        todo.isChecked = !todo.isChecked;
+      }
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <TodoTitle />
